07ce2c6e2e90c8712a095013d93ee3575175c350
shohei-ojs <shohei.ojs@gmail.com>
2019-11-07 02:22:57 +0900

    draw working time on day

--- src/views/Attendance.vue
+++ src/views/Attendance.vue
@@ -3,10 +3,19 @@
     <v-calendar
       type="month"
       :now="today"
-    ></v-calendar>
+    >
+      <template v-slot:day="day">
+        <v-sheet
+          v-if="day.day % 3 === 0"
+        >
+          9:30~<br>18:30
+        </v-sheet>
+      </template>
+    </v-calendar>
     <v-TimePicker label="始まり" value="09:30"></v-TimePicker>
     <v-TimePicker label="終わり" value="18:30"></v-TimePicker>
-    <v-btn large>おやすME (๑´ڡ`๑)てへぺロ♡</v-btn>
+    <v-btn large>おやすME</v-btn>
+    <p>Total:{{sum}}</p>
   </v-sheet>
 </template>
 
@@ -18,7 +27,8 @@ export default {
   },
   data () {
     return {
-      today: new Date().toISOString().substr(0, 10).toString()
+      today: new Date().toISOString().substr(0, 10).toString(),
+      sum: 0
     }
   }
 }
